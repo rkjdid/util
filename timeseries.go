0e3563b1868903c205184bb0bca52d59eca88ef6
rkj <rk@jdid.co>
2017-07-21 10:43:31 +0200

    add naive [int]timeseries implementation w/ observer feat.(!)

---
+++ timeseries.go
@@ -0,0 +1,118 @@
+package util
+
+import (
+	"log"
+	"sync"
+	"time"
+)
+
+const chanSize = 36
+
+// TimeSeries is a naive structure implementation of int timeseries
+// It holds a slice of ints between Start and End times with a maximum length.
+// Each int is supposed to be separated by Interval time, but this is obviously dependant on user.
+type TimeSeries struct {
+	Start    time.Time
+	End      time.Time
+	Interval Duration
+	Data     []int
+
+	maxLength   int
+	subscribers map[int]chan int
+	currentId   int
+	sync.RWMutex
+}
+
+func NewTimeSeries(length int, interval Duration) *TimeSeries {
+	return &TimeSeries{
+		Start:    time.Now(),
+		End:      time.Now(),
+		Interval: interval,
+		Data:     make([]int, 0, length),
+
+		maxLength:   length,
+		subscribers: make(map[int]chan int),
+	}
+}
+
+// Subscribe creates a new buffered chan, adds it to subscribers map for
+// later broadcast on Add() calls. The id of the chan is returned for
+// unsubscribing, and the chan itself.
+func (d *TimeSeries) Subscribe() (id int, data chan int) {
+	if d.subscribers == nil {
+		d.subscribers = make(map[int]chan int)
+	}
+
+	data = make(chan int, chanSize)
+	d.Lock()
+	id = d.currentId
+	d.subscribers[id] = data
+	d.currentId++
+	d.Unlock()
+	return id, data
+}
+
+// Unsubscribe closes and deletes from subscribers channel with id.
+func (d *TimeSeries) Unsubscribe(id int) {
+	d.Lock()
+	ch, ok := d.subscribers[id]
+	if ok {
+		close(ch)
+		delete(d.subscribers, id)
+	}
+	d.Unlock()
+}
+
+// Add value to d.Data. If d.Data length is above d.maxLength, do a 1-shift
+// operation on the slice to the left. Add also assumes user behaves and
+// does a time shift on d.Start of d.Interval duration. After appending value,
+// it is broadcasted to subscribed chans.
+func (d *TimeSeries) Add(v int) {
+	if d.maxLength > 0 && len(d.Data) >= d.maxLength {
+		d.Data = d.Data[1:]
+		// trusting time shift
+		// assumes d.Interval is somehow respected at each Add call
+		d.Start = d.Start.Add(time.Duration(d.Interval))
+	}
+	d.Data = append(d.Data, v)
+	d.End = time.Now()
+	d.broadcast(v)
+}
+
+// Padded returns d.Data left-padded up to d.maxLength with d.Data[0] values.
+func (d *TimeSeries) Padded() []int {
+	if len(d.Data) >= d.maxLength {
+		return d.Data[:d.maxLength]
+	}
+
+	var zero int
+	if len(d.Data) > 0 {
+		zero = d.Data[0]
+	}
+
+	data := make([]int, d.maxLength)
+	for i := 0; i < d.maxLength-len(d.Data); i++ {
+		data[i] = zero
+	}
+	copy(data[d.maxLength-len(d.Data):], d.Data)
+	return data
+}
+
+// ResetStartTime operates a timeshift on Start time from time.Now()
+// of -len(d.Data) * d.Interval
+func (d *TimeSeries) ResetStartTime() {
+	d.Start = time.Now().Add(-time.Duration(int64(len(d.Data)) * int64(d.Interval)))
+}
+
+func (d *TimeSeries) broadcast(v int) {
+	d.Lock()
+	for i, ch := range d.subscribers {
+		if len(ch) == cap(ch) {
+			log.Printf("timeseries: unsubscribing full channel %d", i)
+			delete(d.subscribers, i)
+		} else {
+			ch <- v
+		}
+	}
+	d.Unlock()
+}
---
+++ timeseries_test.go
@@ -0,0 +1,90 @@
+package util
+
+import (
+	"sync"
+	"testing"
+	"time"
+)
+
+func sliceEq(a []int, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func TestDatalog_Add(t *testing.T) {
+	dtest := NewTimeSeries(5, Duration(time.Second))
+
+	dtest.Add(0)
+	expect := []int{0}
+	if !sliceEq(dtest.Data, expect) {
+		t.Errorf("%v != %v", dtest.Data, expect)
+	}
+
+	for i := 1; i < 5; i++ {
+		dtest.Add(i)
+	}
+	expect = []int{0, 1, 2, 3, 4}
+	if !sliceEq(dtest.Data, expect) {
+		t.Errorf("%v != %v", dtest.Data, expect)
+	}
+
+	dtest.Add(5)
+	expect = []int{1, 2, 3, 4, 5}
+	if !sliceEq(dtest.Data, expect) {
+		t.Errorf("%v != %v", dtest.Data, expect)
+	}
+}
+
+func TestDatalog_Padded(t *testing.T) {
+	dtest := NewTimeSeries(5, Duration(time.Second))
+
+	expect := make([]int, 5)
+	if p := dtest.Padded(); !sliceEq(p, expect) {
+		t.Errorf("%v != %v", p, expect)
+	}
+
+	dtest.Add(1)
+	expect = []int{1, 1, 1, 1, 1}
+	if p := dtest.Padded(); !sliceEq(p, expect) {
+		t.Errorf("%v != %v", p, expect)
+	}
+
+	dtest.Add(2)
+	expect[4] = 2
+	if p := dtest.Padded(); !sliceEq(p, expect) {
+		t.Errorf("%v != %v", p, expect)
+	}
+}
+
+func TestDatalog_Subscribe(t *testing.T) {
+	dtest := NewTimeSeries(2, Duration(time.Second))
+	i, ch := dtest.Subscribe()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	expect := []int{}
+	go func() {
+		for x := range ch {
+			expect = append(expect, x)
+		}
+		wg.Done()
+	}()
+
+	dtest.Add(1)
+	dtest.Add(2)
+	dtest.Unsubscribe(i)
+	wg.Wait()
+	if !sliceEq(dtest.Data, []int{1, 2}) {
+		t.Errorf("%v != %v", dtest.Data, expect)
+	}
+	if !sliceEq(dtest.Data, expect) {
+		t.Errorf("%v != %v", dtest.Data, expect)
+	}
+}
