2c1ae723b9864fcb1a798736ef9abf64731e587c
rkj <romainknezevic@gmail.com>
2018-01-06 13:54:32 +0100

    fix dupe var name

--- localized_time.go
+++ localized_time.go
@@ -6,8 +6,8 @@ func ParisTime(t time.Time) time.Time {
 	return LocalizedTime(t, "Europe/Paris")
 }
 
-func LocalizedTime(t time.Time, loc string) time.Time {
-	loc, err := time.LoadLocation(loc)
+func LocalizedTime(t time.Time, s string) time.Time {
+	loc, err := time.LoadLocation(s)
 	if err != nil {
 		return t
 	}
